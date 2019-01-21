d2d9117a283b0d3491c9b347b417095ece1df910
ShunyaOS <support@shunyaos.org>
2019-01-21 18:27:15 +0530

    Improved setup script to install u-Arm python SDK

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
+# insert kernel driver
+# this driver is already placed inside lebian-9-uarm
+# for more Please contact Lemaker support
+
 sudo insmod /lib/modules/cdc-acm.ko
-sudo chmod 777 /dev/ttyACM0
+
+# install u-Arm python SDK
+# install instructions are given on the uArm-Python-SDK Repository
+
+git clone https://github.com/uArm-Developer/uArm-Python-SDK.git
+cd uArm-Python-SDK
+sudo python3 setup.py install 
+
+
+if [ -e /dev/ttyACM0 ];
+then
+
+	sudo chmod 777 /dev/ttyACM0
+else
+	echo "WARNING : "
+	echo "You may not have connected u-Arm to the device."
+	echo "Please connect the device and Run command "
+	echo "sudo chmod 777 /dev/ttyACM0 "
+fi
